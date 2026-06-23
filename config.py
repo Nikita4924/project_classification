856fc4d5e4152d4d5a90d926d614260d477bb5a1
Maksim <presnov.maks2017@yandex.ru>
2026-06-23 18:06:15 +0300

    add a structure and config

---
+++ config.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).parent
+DATA_DIR = ROOT_DIR / "data"
+MODELS_DIR = ROOT_DIR / "models"
+FIGURES_DIR = ROOT_DIR / "reports" / "figures"
+
+# DATA_PATH = DATA_DIR / "data" / ""
+
+# TARGET =
+
+# КОНСТАТЫ
+RANDOM_STATE = 42
+TEST_SIZE = 0.2
